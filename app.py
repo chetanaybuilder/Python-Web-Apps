33ee4bcee67072c46647d9bd22f7bb8485982692
chetanaybuilder <chetanaybatta361@gmail.com>
2026-07-07 16:46:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("index.html")
+    name = "chetanay"
+    age = 15
+    dream = "europe"
+    skills = [
+        "python",
+        "java",
+        "c++"
+    ]
+    projects = True
+    return render_template('index.html', name=name, age=age, dream=dream, skills=skills,projects=projects)
 if __name__ == '__main__':
     app.run(debug=True)
+    
